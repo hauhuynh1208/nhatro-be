033045030f53c32e8e23a17c807389e6ed2b8057
Hau <hau@redcubera.com>
2026-03-31 16:39:47 +0700

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# nhatro-be
+
+NestJS backend scaffold for the nhatro project.
+
+Quick start
+
+1. Change to the folder:
+
+```bash
+cd nhatro-be
+```
+
+2. Install dependencies (run in your environment):
+
+```bash
+npm install
+# or pnpm install / yarn install
+```
+
+3. Start dev server:
+
+```bash
+npm run start:dev
+```
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  // eslint-disable-next-line no-console
+  console.log("Server running on http://localhost:3000");
+}
+
+bootstrap();
